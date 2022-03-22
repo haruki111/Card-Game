1e2fbac863a4a8b805e486f33459671b03e7ac1d
haruki111 <yuji515091@gmail.com>
2022-03-22 22:12:12 +0900

    action round 作成 #2

--- src/views/GameView.vue
+++ src/views/GameView.vue
@@ -37,7 +37,7 @@
       <div id="player" class="text-center">
         <div id="playerInfo" class="text-gray-100">
           <p class="playerName sm:text-3xl text-2xl font-bold mb-2">player1</p>
-          <div class="playerStatus text-base">
+          <div class="playerStatus text-base flex justify-between">
             <p>S:<span id="playerGameStatus">Bet</span></p>
             <p>B:0</p>
             <p>C:400</p>
@@ -71,7 +71,7 @@
       <div id="player" class="text-center mt-24">
         <div id="playerInfo" class="text-gray-100">
           <p class="playerName sm:text-3xl text-2xl font-bold mb-2">player2</p>
-          <div class="playerStatus text-base">
+          <div class="playerStatus text-base flex justify-between">
             <p>S:<span id="playerGameStatus">Bet</span></p>
             <p>B:0</p>
             <p>C:400</p>
@@ -105,7 +105,7 @@
       <div id="player" class="text-center">
         <div id="playerInfo" class="text-gray-100">
           <p class="playerName sm:text-3xl text-2xl font-bold mb-2">player3</p>
-          <div class="playerStatus text-base">
+          <div class="playerStatus text-base flex justify-between">
             <p>S:<span id="playerGameStatus">Bet</span></p>
             <p>B:0</p>
             <p>C:400</p>
@@ -136,6 +136,42 @@
           </div>
         </div>
       </div>
+    </div>
+  </div>
+  <div class="action mt-5 flex justify-center">
+    <button
+      type="button"
+      id="surrenderBtn"
+      class="text-gray-900 bg-white border border-gray-300 hover:bg-gray-100 focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2"
+    >
+      Surrender
+    </button>
+    <button
+      type="button"
+      id="standBtn"
+      class="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2"
+    >
+      Stand
+    </button>
+    <button
+      type="button"
+      id="hitBtn"
+      class="text-white bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2"
+    >
+      Hit
+    </button>
+    <button
+      type="button"
+      id="doubleBtn"
+      class="text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2"
+    >
+      Double
+    </button>
+  </div>
+  <div class="round flex justify-end text-white">
+    <div class="roundTextWrap flex items-center flex-col">
+      <h3 class="text-xl">Round</h3>
+      <p>1 / 5</p>
     </div>
   </div>
   <div class="text-center mt-5">
