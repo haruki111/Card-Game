a010d3817483e638d5b285616ed77cfa3dbb6211
haruki honda <68400191+haruki111@users.noreply.github.com>
2022-03-24 23:11:40 +0900

    component化 (#9)

---
+++ src/components/GameAction.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts"></script>
+<template>
+  <div class="action mt-5 flex justify-center">
+    <button
+      type="button"
+      id="surrenderBtn"
+      class="text-gray-900 bg-white border border-gray-300 hover:bg-gray-100 focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2"
+    >
+      Surrender
+    </button>
+    <button
+      type="button"
+      id="standBtn"
+      class="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2"
+    >
+      Stand
+    </button>
+    <button
+      type="button"
+      id="hitBtn"
+      class="text-white bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2"
+    >
+      Hit
+    </button>
+    <button
+      type="button"
+      id="doubleBtn"
+      class="text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2"
+    >
+      Double
+    </button>
+  </div>
+</template>
---
+++ src/components/PlayerInfo.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts"></script>
+<template>
+  <div id="playerInfo" class="text-gray-100">
+    <p class="playerName sm:text-3xl text-2xl font-bold mb-2">player1</p>
+    <div class="playerStatus text-base flex justify-between">
+      <p>S:Bet</p>
+      <p>B:0</p>
+      <p>C:400</p>
+    </div>
+    <div id="playerScore" class="pb-2">
+      <span
+        class="bg-gray-100 text-gray-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded-full"
+        >50
+      </span>
+    </div>
+  </div>
+</template>
